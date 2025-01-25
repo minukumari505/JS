77cc66a9f3b8962002613538c9fdad736c3d0dd3
minukumari505 <minukumari102004@gmail.com>
2025-01-25 11:40:19 +0000

    container config done

--- 01_Basic/08_DatesinJS.js
+++ 01_Basic/08_DatesinJS.js
@@ -1,40 +1,42 @@
-//Dates
+// //Dates
 
-let myDate = new Date()
-console.log(myDate)
-console.log(myDate.toString())
-console.log(myDate.toDateString())
-console.log(myDate.toISOString())
-console.log(myDate.toJSON())
-console.log(myDate.toLocaleDateString())
-console.log(myDate.toLocaleString())
-console.log(myDate.toTimeString())
-console.log(typeof myDate);
+// let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toTimeString())
+// console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2025,0,23)
-console.log(myCreatedDate);
-// let myCreatedDate = new Date(2025,0,23,5,3)
-// let myCreatedDate = new Date("2025-01-25")
-let myCreatedDate = new Date("01-25-2025")
-console.log(myCreatedDate.toDateString());
-console.log(myCreatedDate.toLocaleDateString());
+// // let myCreatedDate = new Date(2025,0,23)
 
-let myTimeStamp = Date.now()
-console.log(myTimeStamp);  //give millisecond value from 1 jan 1970
-console.log(myCreatedDate.getTime());
-console.log(Math.floor(Date.now()/1000));
+// // let myCreatedDate = new Date(2025,0,23,5,3)
+// // let myCreatedDate = new Date("2025-01-25")
+// let myCreatedDate = new Date("01-25-2025")
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);  //give millisecond value from 1 jan 1970
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth());     //console.log(newDate.getMonth()+1);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth());     //console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
 
-`${newDate.getDay()} and the time`
+console.log(`${newDate.getDay()} and the time is 3:10`)
 
 newDate.toLocaleString('default',{ 
     weekday: "long",
     // timeZone: ''
 
 })
+console.log(newDate.toString('default'));
 
 
---
+++ 02_Basic/01_Array.js
@@ -0,0 +1,35 @@
+// // array
+
+const myarr = [0,1,2,3,4,5]
+// const myHeros = ["shaktiman","naagraj"]
+
+// const myarr2 = new Array(1,2,45,7,8)
+// console.log(myarr[0]);
+
+// //Arrays Methods
+// myarr.push(6);
+// myarr.push(7);
+// myarr.pop();
+// myarr.unshift(9)
+// myarr.shift();
+// console.log(myarr);
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(3));
+// console.log(myarr)
+
+// const newArr = myarr.join()
+
+// console.log(myarr);
+// console.log(newArr);
+// console.log(typeof newArr)
+
+//slice and splice
+console.log("A ", myarr);
+const myn1 = myarr.slice(1,3)
+console.log(myn1);
+console.log(" B",myarr);
+
+const myn2 = myarr.splice(1,3);
+console.log(" C", myarr)
+console.log(myn2);
